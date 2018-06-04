1fc01bb4a62cad3ec5d3797d0ca18066102a36ef
fegumi2003 <fegumi2003@gmail.com>
2018-06-03 20:26:33 -0600

    Create ssh_server.pp

---
+++ site/profile/manifests/ssh_server.pp
@@ -0,0 +1,16 @@
+class profile::ssh_server {
+  package {'openssh-server':
+    ensure => present,
+  }
+  service {'sshd':
+    ensure => 'runnning',
+    ensure => 'true',
+  }
+  ssh_authorized_key { 'root@master.puppet.vm':
+    ensure => present,
+    user => 'root',
+    type => 'ssh-rsa',
+    key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC9FCQvceK+v8wyDTiRaLc/Xj7XrxqvHSkp2V1Oc92lNAu/rZk8QWse9mGjbenhMHmuDeolNMeKNhj7wNK4cO365DQpCPsOCcUIVgVoZi+eHlNIfLYY17DPdCiZnn71ZEgsKuA6p0oBl6hS8XeCZqlF6gIAWpgS+xFj5YYFxMYICl8q2wljS9Hr9hOxuEclUNYqMdtYr0I/86JmfY8nQYwzisgVPbED/aIipcnmRoOlqgHT5mqrl+YI61ZwUpzmH7xVAaO+Y6+1ZyLc9umweRe53V/CofSDxNoWbT1dT0mZ5rQwHQXKTXTpjCrniOKxE/uRJVaUI4sdFqG2JnjVBmsX',
+  }
+}
+    
